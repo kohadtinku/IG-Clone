590024d21a419034853b228b30cc749d631d1005
tinku kohad <tinkukohad05@gmail.com>
2024-08-29 14:37:28 +0530

    ready for deployment

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
+
+import path from "path";
+
+dotenv.config({});
+
+const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
+
+// app.get("/", (req, res) => {
+//   return res.status(200).json({
+//     message: "I am Backend",
+//     success: true,
+//   });
+// });
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credential: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
+
+
+server.listen(PORT, () => {
+  connectDB();
+  console.log(`server listen on port ${PORT}`);
+});
---
+++ frontend/src/components/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import LeftSidebar from './LeftSidebar'
+
+
+const MainLayout = () => {
+  return (
+    <>
+    <LeftSidebar/>
+    <div>
+        <Outlet/>
+    </div>
+    </>
+  )
+}
+
+export default MainLayout
